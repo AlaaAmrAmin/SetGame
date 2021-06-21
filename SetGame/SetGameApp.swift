681fcf5cad897d883972d6eab7740959355bdd18
Alaa' Amr Amin <alaa.amin@vodafone.com>
2021-06-21 16:37:09 +0200

    initial commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Alaa' Amr Amin on 21/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
